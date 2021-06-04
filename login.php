0ca54b1438b981928776afb8fc96f48295e82704
niceducation <85287087+niceducation@users.noreply.github.com>
2021-06-04 12:38:05 +0530

    Update login.php

--- login.php
+++ login.php
@@ -15,6 +15,7 @@
     <!-- Place favicon.ico in the root directory -->
 
     <!-- CSS here -->
+      <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/css/animate.min.css">

e4a672a51cf20479f14d86ebf7b917e4564080d0
niceducation <85287087+niceducation@users.noreply.github.com>
2021-06-07 13:19:07 +0530

    Add files via upload

---
+++ mystudy/login.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+
+<!-- Mirrored from www.devsnews.com/template/micoach/micoach/courses -2.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 09 Feb 2021 05:29:24 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>MiCoach - Online Courses HTML5 Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.html">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/metisMenu.css">
+    <link rel="stylesheet" href="assets/css/meanmenu.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/main1.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+  
+
+   <header class="header-transparent">
+        <div id="sticky-header" class="main-menu-area menu-padding pl-55 pr-55">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-xl-2 col-lg-7 col-md-6 col-8 py-2">
+                        <div class="logo">
+                            <a href="https://nicedu.org"><img src="assets/img/logo/nicedu.png" width="180px" alt="" /></a>
+                        </div>
+                    </div>
+                    <div class="col-xl-10 col-lg-5 col-md-6 col-4">
+                        <div class="header-right d-sm-flex align-items-center justify-content-end">
+                            <div class="header-sing d-none d-md-inline-block shape">
+                                <a class="shape" href="login.php"><i class="fa fa-user iconcolor" aria-hidden="true"></i> Login</a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+    
+   
+    <main>
+        <section class="space-3 background">
+            <div class="container mr-auto pt-180 pb-100 mobilepadd">
+                <div class="row">
+                    <div class="col-md-4 col-12 col-sm-12">
+                        <div class="woocommerce-notices-wrapper"></div>
+                        <h2 class="font-weight-bold mb-4 d-flex justify-content-center align-items-center headicon"><img class="icon2" src="assets/img/gallery/game.png"> Login</h2>
+                        <form action=""  class="woocommerce-form woocommerce-form-login login" name="login" method="post">
+                               <p class=" form-row form-row-wide">
+                                <input type="text" class="input-text form-control" placeholder="Email address" name="username" required >
+                            </p>
+                            <p class="form-row form-row-wide">
+                                <input class=" input-text form-control" type="password" name="Password" placeholder="Password" required >
+                            </p>
+                                <div class="padd"><div data-sitekey="6LeVnLgZAAAAACSO9UojXrMYhgfAcEv5-5op1204" class="g-recaptcha"></div></div>
+                        
+                            <p class="form-row">
+                                <button type="submit" name="submit" class="c-btn btn-theme mt-3"  value="Login"> Log in</button>
+                                
+                            </p>
+                            <p class="woocommerce-LostPassword lost_password">
+                                <a href="forgot-password.html">Lost your password?</a>
+                            </p>
+                        </form>
+                    </div>
+                    <div class="col-md-7  offset-md-1 ">
+                        <h2 class="font-weight-bold mb-3 d-flex justify-content-center align-items-center"><img class="icon2" src="assets/img/gallery/game.png"> Register</h2>
+                        <form method="post" action="https://nicemarg.org/register" class="woocommerce-form woocommerce-form-register register">
+                            <input type="hidden" name="_token" value="BeglJSXD8yLvZ7JdLaXZtiznG9eHMraLN1aNSgqi">                            <div class="row">
+                               
+                                <div class="col-12 col-sm-12 col-lg-6 col-md-6" >
+                                    <p class=" form-row form-row-wide margi margi1">
+                                        <input type="text" class=" input-text form-control" name="name" placeholder="Full Name"  id="name" autocomplete="name" required >
+                                    </p>
+                                </div>
+                                 <div class="col-12 col-sm-12 col-lg-6 col-md-6" >
+                                 <p class=" form-row form-row-wide margi">
+                                        <input type="email" class=" input-text form-control" name="email"  autocomplete="email" id="email" placeholder="Email">
+                                    </p></div>
+                                
+                                <div class="col-12 col-sm-12 col-lg-6 col-md-6" >
+                                    <p class=" form-row form-row-wide">
+                                        <input type="text" class=" input-text form-control" placeholder="Mobile" name="mobile" id="mobile" autocomplete="off" pattern="^[0-9]{10}$"  maxlength="10" required >
+                                    </p>
+                                </div>
+                                <div class="col-12 col-sm-12 col-lg-4 col-md-6"  >
+                                    <p class=" form-row form-row-wide">
+                                        <input type="text" class=" input-text form-control" name="otp" placeholder="OTP"  autocomplete="off" >
+                                        <p id="otpError"></p>
+                                    </p>
+                                </div>
+                                <div class="col-12 col-sm-12 col-lg-2  col-md-6 " >
+                                    <p class=" form-row form-row-wide">
+                                        <button id="mobileOTPVer" type="button" class="c-btn btn-theme">Verify</button>
+                                    </p>
+                                </div>
+                                <div class="col-12 col-sm-12 col-lg-6 col-md-6">
+                                    <div data-sitekey="6LeVnLgZAAAAACSO9UojXrMYhgfAcEv5-5op1204" class="g-recaptcha"></div>
+                                                                
+                            </div>
+                                <div class="col-12 col-sm-12 col-lg-6 col-md-6 p-0 m-0">
+                                        <div class="row">
+                                <div class="col-12 col-sm-12 col-lg-12 col-md-12" >
+                                        <p id="emailError" ></p>
+                                        <p>The Password will be sent to your email.</p>
+                                    </p>
+                                </div>
+                                <div class="col-12 col-sm-12 col-lg-12 col-md-12">
+                                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" required checked>
+  <label for="vehicle1"> I agree to Terms of Service and Privacy Policy</label><br>
+                                    <div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                            
+                                
+                            
+                            <p class="woocommerce-FormRow form-row">
+                                <input type="hidden" id="woocommerce-register-nonce" name="woocommerce-register-nonce" value="b1c661ab82"><input type="hidden" name="_wp_http_referer" value="/my-account/">
+                                <button type="submit " class="c-btn btn-theme ml-3" id="register" name="register" value="Register">Register</button>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--shop category end-->
+        <!-- footer-area-start -->
+
+
+
+
+
+        <!-- footer-area-start -->
+        <footer class="pt-3">
+        	<div class="container">
+        	<div class="row mb-25 no-gutters">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-12">
+                        <div class="footer-logo">
+                         <a href="index.php"><img src="assets/img/logo/nicedu.png" width="180px"></a>
+                        </div>
+                    </div>
+                    <div class="col-xl-8 col-lg-8 col-md-8 col-12 pt-4">
+                        <div class="footer-social-icon text-right pb-70">
+                            <a class="fot-fb active pb-70 paddo resp" href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+                            <a class="fot-twitter pb-70" href="#"><i class="fab fa-twitter"></i> Twitter</a>
+                            <a class="fot-google pb-70" href="#"><i class="fab fa-youtube"></i> Youtube</a>
+                            <a class="fot-insta pb-70" href="#"><i class="fab fa-instagram"></i> Instagram</a>
+                        </div>
+                </div>
+                </div>
+            </div>
+        	
+
+
+
+
+
+                </div>
+                <div class="copyright-area mt-15 text-center pt-20 pb-20">
+                    <div class="row mr-0">
+                        <div class="col-xl-12">
+                            <div class="copyright-text">
+                                <p>Copyright © 2020 <a href="index.php">Nice Education</a> All Rights Reserved.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer-area-end -->
+
+    </main>
+
+
+
+    <!-- JS here -->
+
+    <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/metisMenu.min.js"></script>
+    <script src="assets/js/jquery.meanmenu.min.js"></script>
+    <script src="assets/js/ajax-form.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/jquery.scrollUp.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.nice-select.js"></script>
+    <script src="assets/js/jquery.easypiechart.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js' async defer ></script>
+
+</body>
+
+
+<!-- Mirrored from www.devsnews.com/template/micoach/micoach/courses -2.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 09 Feb 2021 05:29:24 GMT -->
+</html>
